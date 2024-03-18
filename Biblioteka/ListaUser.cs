ccb90ea05506e7fbf61ddb435dfd9caf9683fb31
dawkrusz <kruszynski.dd@gmail.com>
2024-03-18 16:05:11 +0100

    ListeUser added the necessary tables according to use cases

--- Biblioteka/ListaUser.cs
+++ Biblioteka/ListaUser.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Biblioteka
 {
@@ -21,12 +19,25 @@ namespace Biblioteka
         private void SetupListView()
         {
             listView1.View = View.Details;
+
+            // Dodaj kolumny dla każdego pola z bazy danych
             listView1.Columns.Add("ID", 50);
             listView1.Columns.Add("Imię", 100);
             listView1.Columns.Add("Nazwisko", 150);
             listView1.Columns.Add("Email", 200);
-            listView1.Columns.Add("login", 250);
+            listView1.Columns.Add("Telefon", 100);
+            listView1.Columns.Add("Miejscowość", 100);
+            listView1.Columns.Add("Kod pocztowy", 80);
+            listView1.Columns.Add("Ulica", 150);
+            listView1.Columns.Add("Numer posesji", 100);
+            listView1.Columns.Add("Numer lokalu", 100);
+            listView1.Columns.Add("PESEL", 100);
+            listView1.Columns.Add("Data urodzenia", 100);
+            listView1.Columns.Add("Płeć", 80);
+            listView1.Columns.Add("Login", 100);
+            listView1.Columns.Add("Hasło", 150);
         }
+
         private void PopulateListView()
         {
             listView1.Items.Clear();
@@ -36,7 +47,7 @@ namespace Biblioteka
                 try
                 {
                     connection.Open();
-                    string query = "SELECT * FROM klient";
+                    string query = "SELECT * FROM uzytkownik";
 
                     using (MySqlCommand command = new MySqlCommand(query, connection))
                     {
@@ -44,12 +55,23 @@ namespace Biblioteka
                         {
                             while (reader.Read())
                             {
-                                ListViewItem item = new ListViewItem(reader["id_klient"].ToString());
-                                item.SubItems.Add(reader["k_imie"].ToString());
-                                item.SubItems.Add(reader["k_nazwisko"].ToString());
-                                item.SubItems.Add(reader["k_email"].ToString());
-                                item.SubItems.Add(reader["k_login"].ToString());
-                                item.SubItems.Add(reader["k_haslo"].ToString());
+                                ListViewItem item = new ListViewItem(reader["id_uzytkownik"].ToString());
+
+                                // Dodaj wartości dla każdego pola z bazy danych
+                                item.SubItems.Add(reader["u_imie"].ToString());
+                                item.SubItems.Add(reader["u_nazwisko"].ToString());
+                                item.SubItems.Add(reader["u_email"].ToString());
+                                item.SubItems.Add(reader["u_telefon"].ToString());
+                                item.SubItems.Add(reader["u_miejscowosc"].ToString());
+                                item.SubItems.Add(reader["u_kod"].ToString());
+                                item.SubItems.Add(reader["u_ulica"].ToString());
+                                item.SubItems.Add(reader["u_nr_posesji"].ToString());
+                                item.SubItems.Add(reader["u_nr_lokalu"].ToString());
+                                item.SubItems.Add(reader["u_pesel"].ToString());
+                                item.SubItems.Add(reader["u_data_ur"].ToString());
+                                item.SubItems.Add(reader["u_plec"].ToString());
+                                item.SubItems.Add(reader["u_login"].ToString());
+                                item.SubItems.Add(reader["u_haslo"].ToString());
 
                                 listView1.Items.Add(item);
                             }
@@ -62,5 +84,6 @@ namespace Biblioteka
                 }
             }
         }
+
     }
 }
