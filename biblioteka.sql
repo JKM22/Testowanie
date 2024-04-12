e309332f01e2d2f724d051b3816f53239ce915c3
mMartynka <155664139+mMartynka@users.noreply.github.com>
2024-04-12 10:55:14 +0200

    Update biblioteka.sql

--- biblioteka.sql
+++ biblioteka.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2024 at 05:50 PM
+-- Generation Time: Apr 12, 2024 at 10:54 AM
 -- Wersja serwera: 10.4.32-MariaDB
--- Wersja PHP: 8.0.30
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -75,6 +75,26 @@ CREATE TABLE `ksiazka` (
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `pary_uprawnienia`
+--
+
+CREATE TABLE `pary_uprawnienia` (
+  `id_pary` int(11) NOT NULL,
+  `id_uprawnienia` int(11) DEFAULT NULL,
+  `id_uzytkownik` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pary_uprawnienia`
+--
+
+INSERT INTO `pary_uprawnienia` (`id_pary`, `id_uprawnienia`, `id_uzytkownik`) VALUES
+(30, 2, 2),
+(79, 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `uprawnienia`
 --
 
@@ -90,7 +110,10 @@ CREATE TABLE `uprawnienia` (
 INSERT INTO `uprawnienia` (`id_uprawnienia`, `uprawnienia`) VALUES
 (1, 'Administrator'),
 (2, 'Użytkownik zalogowany'),
-(3, 'Użytkownik niezalogowany');
+(3, 'Zmień uprawnienia'),
+(4, 'Modyfikacja użytkownika'),
+(5, 'Zmień hasło'),
+(6, 'Przeglądaj roboczy');
 
 -- --------------------------------------------------------
 
@@ -113,18 +136,17 @@ CREATE TABLE `uzytkownik` (
   `u_data_ur` date NOT NULL,
   `u_plec` varchar(10) NOT NULL,
   `u_login` varchar(100) NOT NULL,
-  `u_haslo` varchar(100) NOT NULL,
-  `id_uzytkownik_uprawnienia` int(11) DEFAULT NULL
+  `u_haslo` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
 -- Dumping data for table `uzytkownik`
 --
 
-INSERT INTO `uzytkownik` (`id_uzytkownik`, `u_imie`, `u_nazwisko`, `u_email`, `u_telefon`, `u_miejscowosc`, `u_kod`, `u_ulica`, `u_nr_posesji`, `u_nr_lokalu`, `u_pesel`, `u_data_ur`, `u_plec`, `u_login`, `u_haslo`, `id_uzytkownik_uprawnienia`) VALUES
-(1, 'Jakub', 'Kowalski', 'mail@mail.com', 123456789, 'Rogów', '00-001', 'ul. Wielka', '33', '', '73110743682', '1994-03-09', 'Kobieta', 'kubakowal123', 'db3fc40e6439d4d972870252ccc72f62', 1),
-(2, 'Joanna', 'Wilk', 'mail123@mail.com', 987654321, 'Warszawa', '00-002', 'ul. Łódzka', '14', '21', '00220879196', '2004-08-20', 'Kobieta', 'asiaaisa', '241190129fd7bab5b857c600dac6e2c5', 2),
-(3, 'Mariusz', 'Pudzianowski', 'pudzian@mail.com', 123123123, 'Poznań', '00-003', 'ul. Mała', '12', NULL, '87080361313', '1997-02-07', 'Mężczyzna', 'pudzianator', '7c8a7c5f8338b02d82840d64e23e47e6', 1);
+INSERT INTO `uzytkownik` (`id_uzytkownik`, `u_imie`, `u_nazwisko`, `u_email`, `u_telefon`, `u_miejscowosc`, `u_kod`, `u_ulica`, `u_nr_posesji`, `u_nr_lokalu`, `u_pesel`, `u_data_ur`, `u_plec`, `u_login`, `u_haslo`) VALUES
+(1, 'Jakub', 'Kowalski', 'mail@mail.com', 123456789, 'Rogów', '00-001', 'ul. Wielka', '33', '', '73110743682', '1994-03-09', 'Kobieta', 'kubakowal123', 'db3fc40e6439d4d972870252ccc72f62'),
+(2, 'Joanna', 'Wilk', 'mail123@mail.com', 987654321, 'Warszawa', '00-002', 'ul. Łódzka', '14', '21', '00220879196', '2004-08-20', 'Kobieta', 'asiaaisa', '241190129fd7bab5b857c600dac6e2c5'),
+(3, 'Mariusz', 'Pudzianowski', 'pudzian@mail.com', 123123123, 'Poznań', '00-003', 'ul. Mała', '12', NULL, '87080361313', '1997-02-07', 'Mężczyzna', 'pudzianator', '7c8a7c5f8338b02d82840d64e23e47e6');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -153,6 +175,14 @@ ALTER TABLE `ksiazka`
   ADD KEY `autor` (`autor`);
 
 --
+-- Indeksy dla tabeli `pary_uprawnienia`
+--
+ALTER TABLE `pary_uprawnienia`
+  ADD PRIMARY KEY (`id_pary`),
+  ADD KEY `id_uprawnienia` (`id_uprawnienia`),
+  ADD KEY `id_uzytkownik` (`id_uzytkownik`);
+
+--
 -- Indeksy dla tabeli `uprawnienia`
 --
 ALTER TABLE `uprawnienia`
@@ -162,8 +192,7 @@ ALTER TABLE `uprawnienia`
 -- Indeksy dla tabeli `uzytkownik`
 --
 ALTER TABLE `uzytkownik`
-  ADD PRIMARY KEY (`id_uzytkownik`),
-  ADD KEY `id_uprawnienia` (`id_uzytkownik_uprawnienia`);
+  ADD PRIMARY KEY (`id_uzytkownik`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -188,10 +217,16 @@ ALTER TABLE `ksiazka`
   MODIFY `id_ksiazka` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `pary_uprawnienia`
+--
+ALTER TABLE `pary_uprawnienia`
+  MODIFY `id_pary` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+
+--
 -- AUTO_INCREMENT for table `uzytkownik`
 --
 ALTER TABLE `uzytkownik`
-  MODIFY `id_uzytkownik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_uzytkownik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -205,10 +240,11 @@ ALTER TABLE `ksiazka`
   ADD CONSTRAINT `ksiazka_ibfk_2` FOREIGN KEY (`autor`) REFERENCES `autor` (`id_autor`);
 
 --
--- Constraints for table `uzytkownik`
+-- Constraints for table `pary_uprawnienia`
 --
-ALTER TABLE `uzytkownik`
-  ADD CONSTRAINT `uzytkownik_ibfk_1` FOREIGN KEY (`id_uzytkownik_uprawnienia`) REFERENCES `uprawnienia` (`id_uprawnienia`);
+ALTER TABLE `pary_uprawnienia`
+  ADD CONSTRAINT `pary_uprawnienia_ibfk_1` FOREIGN KEY (`id_uprawnienia`) REFERENCES `uprawnienia` (`id_uprawnienia`),
+  ADD CONSTRAINT `pary_uprawnienia_ibfk_2` FOREIGN KEY (`id_uzytkownik`) REFERENCES `uzytkownik` (`id_uzytkownik`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
