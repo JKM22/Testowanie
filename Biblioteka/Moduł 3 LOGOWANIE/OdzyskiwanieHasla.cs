b4fc397b1c987cba9202b0efa8ccaf184f878ce8
dawkrusz <kruszynski.dd@gmail.com>
2024-04-29 02:55:19 +0200

    validation for generated passwords and now email password reset works

--- Biblioteka/Moduł 3 LOGOWANIE/OdzyskiwanieHasla.cs
+++ Biblioteka/Moduł 3 LOGOWANIE/OdzyskiwanieHasla.cs
@@ -4,7 +4,6 @@ using ComponentFactory.Krypton.Toolkit;
 using System.Net;
 using System.Net.Mail;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Biblioteka
@@ -35,7 +34,7 @@ namespace Biblioteka
 
             if (polaczenie.UpdatePassword(email, newPassword, true))
             {
-                await SendEmailAsync(email, "New Password", $"Your new password is: {newPassword}");
+                SendEmail(newPassword);
                 this.Hide();
                 Login loginForm = new Login();
                 loginForm.Show();
@@ -48,32 +47,75 @@ namespace Biblioteka
 
         private string GeneratePassword()
         {
-            string uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-            string digits = "0123456789";
-            string specialCharacters = "-_!*#$&";
+            bool walidacja_pass = false;
+            do
+            {
+                string uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                string lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+                string digits = "0123456789";
+                string specialCharacters = "-_!*#$&";
 
-            Random random = new Random();
+                Random random = new Random();
 
-            string randomUppercase = new string(Enumerable.Repeat(uppercaseLetters, 3)
-                                                          .Select(s => s[random.Next(s.Length)]).ToArray());
-            string randomLowercase = new string(Enumerable.Repeat(lowercaseLetters, 3)
-                                                          .Select(s => s[random.Next(s.Length)]).ToArray());
-            string randomDigits = new string(Enumerable.Repeat(digits, 2)
-                                                       .Select(s => s[random.Next(s.Length)]).ToArray());
-            string randomSpecialCharacters = new string(Enumerable.Repeat(specialCharacters, 2)
-                                                                  .Select(s => s[random.Next(s.Length)]).ToArray());
+                string randomUppercase = new string(Enumerable.Repeat(uppercaseLetters, 3)
+                                                              .Select(s => s[random.Next(s.Length)]).ToArray());
+                string randomLowercase = new string(Enumerable.Repeat(lowercaseLetters, 3)
+                                                              .Select(s => s[random.Next(s.Length)]).ToArray());
+                string randomDigits = new string(Enumerable.Repeat(digits, 2)
+                                                           .Select(s => s[random.Next(s.Length)]).ToArray());
+                string randomSpecialCharacters = new string(Enumerable.Repeat(specialCharacters, 2)
+                                                                      .Select(s => s[random.Next(s.Length)]).ToArray());
 
-            string combinedCharacters = randomUppercase + randomLowercase + randomDigits + randomSpecialCharacters;
+                string combinedCharacters = randomUppercase + randomLowercase + randomDigits + randomSpecialCharacters;
 
-            string shuffledPassword = new string(combinedCharacters.ToCharArray().OrderBy(c => random.Next()).ToArray());
+                string shuffledPassword = new string(combinedCharacters.ToCharArray().OrderBy(c => random.Next()).ToArray());
+                //walidacja
+                bool hasUpperCase = shuffledPassword.Any(char.IsUpper);
+                bool hasLowerCase = shuffledPassword.Any(char.IsLower);
+                bool hasDigit = shuffledPassword.Any(char.IsDigit);
+                bool hasSpecialChar = shuffledPassword.Any(ch => !char.IsLetterOrDigit(ch));
 
-            return shuffledPassword;
+                if (shuffledPassword.Length == 10)
+                {
+                    walidacja_pass = false;
+                }
+                else if (!hasUpperCase || !hasLowerCase || !hasDigit || !hasSpecialChar)
+                {
+                    walidacja_pass = false;
+                }
+                else
+                {
+                    walidacja_pass = true;
+                }
+                return shuffledPassword;
+            } while (walidacja_pass == false);
         }
 
-        private async Task SendEmailAsync(string recipient, string subject, string body)
+        private void SendEmail(string newPassword)
         {
-            
+                var fromMail = new MailAddress("bibliotekapasswordreset@outlook.com", "Zmiana hasła biblioteka");
+                var toMail = new MailAddress(textBoxEmail.Text);
+
+                string subject = "Resetowanie Hasła";
+                string body = $"Twoje nowe hasło to: {newPassword}";
+
+                string email = "bibliotekapasswordreset@outlook.com";
+                string haslo = "alamakota123";
+                string Host = "smtp.office365.com";
+                int Port = 587;
+
+                using (MailMessage mail = new MailMessage(fromMail.ToString(), toMail.ToString(), subject, body))
+                {
+                    using (SmtpClient smtp = new SmtpClient(Host, Port))
+                    {
+                        smtp.UseDefaultCredentials = false;
+                        smtp.EnableSsl = true;
+                        smtp.Credentials = new NetworkCredential(email, haslo);
+                        smtp.Send(mail);
+                        MessageBox.Show("Hasło zostało zresetowane. \n" +
+                            "Wiadomość została wysłana na adres email, w przypadku braku wiadomości sprawdź spam.", "Success");
+                    }
+                }
         }
 
         private void button_wroc_Click(object sender, EventArgs e)
