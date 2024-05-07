0699087847f487b3dfff658ec57bde93fc126be0
dawkrusz <kruszynski.dd@gmail.com>
2024-05-07 11:47:48 +0200

    added label informing the user what should the password have (nowehaslo.cs)

--- Biblioteka/Moduł 3 LOGOWANIE/NoweHaslo.Designer.cs
+++ Biblioteka/Moduł 3 LOGOWANIE/NoweHaslo.Designer.cs
@@ -36,6 +36,7 @@
             this.textBoxNewPass = new System.Windows.Forms.TextBox();
             this.textBoxNewPass2 = new System.Windows.Forms.TextBox();
             this.buttonSavePass = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // kryptonPalette1
@@ -76,20 +77,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Roboto Slab Medium", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(25, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.Size = new System.Drawing.Size(81, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "NOWE HASŁO";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Roboto Slab Medium", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(3, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 15);
+            this.label1.Size = new System.Drawing.Size(103, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "POWTÓRZ HASŁO";
             // 
@@ -111,7 +112,7 @@
             // 
             // buttonSavePass
             // 
-            this.buttonSavePass.Font = new System.Drawing.Font("Roboto Medium", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSavePass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.buttonSavePass.Location = new System.Drawing.Point(109, 136);
             this.buttonSavePass.Name = "buttonSavePass";
             this.buttonSavePass.Size = new System.Drawing.Size(125, 23);
@@ -120,12 +121,24 @@
             this.buttonSavePass.UseVisualStyleBackColor = true;
             this.buttonSavePass.Click += new System.EventHandler(this.buttonSavePass_Click_1);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 78);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hasło musi posiadać:\r\n- od 8 do 15 znaków\r\n- przynajmniej jedną małą literę\r\n- pr" +
+    "zynajmniej jedną wielką literę\r\n- przynajmniej jedną cyfrę\r\n- przynajmniej jeden" +
+    " znak specjalny\r\n";
+            // 
             // NoweHaslo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
             this.ClientSize = new System.Drawing.Size(246, 349);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.buttonSavePass);
             this.Controls.Add(this.textBoxNewPass2);
             this.Controls.Add(this.textBoxNewPass);
@@ -149,5 +162,6 @@
         private System.Windows.Forms.TextBox textBoxNewPass;
         private System.Windows.Forms.TextBox textBoxNewPass2;
         private System.Windows.Forms.Button buttonSavePass;
+        private System.Windows.Forms.Label label3;
     }
 }
